02d1882317f2c2f1c67e11946f1bd0943880fea0
Suraj-1309 <surajsinghch2055@gmail.com>
2026-03-25 19:14:31 +0530

    add data api to send and download data

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.routes.hello import router as hello_router
+from app.api.routes.data import router as data_router
 
 app = FastAPI(title="Landslide Prediction API")
 
 app.include_router(hello_router)
+app.include_router(data_router)
